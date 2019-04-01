f56227e5388b870d0fa526103caa4c5c867dedd5
stefaniemartins <stefanie.m@aluno.ifsc.edu.br>
2019-04-01 14:38:33 -0300

    Criado o javadoc das classes.

--- src/poo/Tempo.java
+++ src/poo/Tempo.java
@@ -1,5 +1,10 @@
 package poo;
 
+/**
+ * Classe para representar o tempo.
+ *
+ * @author Stefanie Martins.
+ */
 public class Tempo
 {
     private int horas;
@@ -8,7 +13,9 @@ public class Tempo
     private String horario;
     private long horario_segundos;
 
-
+    /**
+     * Construtor que não recebe parâmetro.
+     */
     public Tempo()
     {
         horas = 0; //Só guarda 0 porque como é int ele ignora os zeros a esquerda. Na hora de imprimir em String deve ser impresso 00.
@@ -16,17 +23,33 @@ public class Tempo
         segundos = 0;
     }
 
+
+    /**
+     * Construtor que recebe as horas como parâmentro.
+     * @param horas recebe as horas informadas.
+     */
     public Tempo(int horas)
     {
         setHoras(horas);
     }
 
+    /**
+     * Construtor que recebe as horas e os minutos como parâmentro.
+     * @param horas recebe as horas informadas.
+     * @param minutos recebe os minutos informados.
+     */
     public Tempo(int horas, int minutos)
     {
         setHoras(horas);
         setMinutos(minutos);
     }
 
+    /**
+     * Construtor que recebe as horas, os minutos e os segundos como parâmentro.
+     * @param horas recebe as horas informadas.
+     * @param minutos recebe os minutos informados.
+     * @param segundos recebe os segundos informados.
+     */
     public Tempo(int horas, int minutos, int segundos)
     {
         setHoras(horas);
@@ -34,6 +57,11 @@ public class Tempo
         setSegundos(segundos);
     }
 
+    /**
+     * O método altera as horas.
+     * @param horas recebe as horas informadas.
+     * @return true caso tenha conseguido alterar o atributo horas, retorna false caso não tenha consegudio alterar o atributo horas.
+     */
     public boolean setHoras(int horas)
     {
         if ((horas < 0) || (horas > 23))
@@ -49,6 +77,11 @@ public class Tempo
         }
     }
 
+    /**
+     * O método altera os minutos.
+     * @param minutos recebe os minutos informados.
+     * @return true caso tenha conseguido alterar o atributo minutos, retorna false caso não tenha consegudio alterar o atributo minutos.
+     */
     public boolean setMinutos(int minutos)
     {
         if ((minutos < 0) || (minutos > 59))
@@ -64,6 +97,11 @@ public class Tempo
         }
     }
 
+    /**
+     * O método altera os segundos.
+     * @param segundos recebe os segundos informados.
+     * @return true caso tenha conseguido alterar o atributo segundos, retorna false caso não tenha consegudio alterar o atributo segundos.
+     */
     public boolean setSegundos(int segundos)
     {
         if ((segundos < 0) || (segundos > 59))
@@ -79,21 +117,38 @@ public class Tempo
         }
     }
 
+    /**
+     * O método obtém as horas.
+     * @return o atributo horas.
+     */
     public int getHoras()
     {
             return horas;
     }
 
+    /**
+     * O método obtém os minutos.
+     * @return o atributo minutos.
+     */
     public int getMinutos()
     {
         return minutos;
     }
 
+    /**
+     * O método obtém os segundos.
+     * @return o atributo segundos.
+     */
     public int getSegundos()
     {
         return segundos;
     }
 
+    /**
+     * O método concatena os atributos horas, minutos e segundos para um padrão. Caso um dos atributos seja menor que 10,
+     * será acrescentado um 0 a frente do atributo apra manter o padrão.
+     * @return uma String com o formato de horário hh:mm:ss.
+     */
     @Override
     public String toString()
     {
@@ -136,6 +191,10 @@ public class Tempo
         return horario;
     }
 
+    /**
+     * O método converte os atributos horas e minutos para segundos. Após isso, ele soma os três atributos: horas, minutos e segundos.
+     * @return o atributo horario_segundos (o horário convertido totalmente em segundos),
+     */
     public long getHorario_segundos()
     {
         horario_segundos = segundos;
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"poo","l":"Principal"},{"p":"poo","l":"Tempo"}]
